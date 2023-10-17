2c6cbde84290132ca812e5be7b42fc65ec4320af
Maciej Mozolewski <m.mozolewski@gmail.com>
2023-10-17 21:52:00 +0200

    Lab02 LeapYearCalculatorTest fails

---
+++ src/main/java/org/leapyear/LeapYearCalculator.java
@@ -0,0 +1,9 @@
+package org.leapyear;
+
+public class LeapYearCalculator {
+
+  public static boolean isLeapYear(int year) {
+    // Your implementation here
+    return false;
+  }
+}
---
+++ src/test/java/org/leapyear/LeapYearCalculatorTest.java
@@ -0,0 +1,26 @@
+package org.leapyear;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class LeapYearCalculatorTest {
+
+  @Test
+  void testLeapYear() {
+    assertTrue(LeapYearCalculator.isLeapYear(1600));  // 1600 is a leap year
+    assertTrue(LeapYearCalculator.isLeapYear(2000));  // 2000 is a leap year
+  }
+
+  @Test
+  void testNonLeapYear() {
+    assertFalse(LeapYearCalculator.isLeapYear(1700)); // 1700 is not a leap year
+    assertFalse(LeapYearCalculator.isLeapYear(2017)); // 2017 is not a leap year
+  }
+
+  @Test
+  void testOutOfRangeYear() {
+    assertFalse(LeapYearCalculator.isLeapYear(-1600));// -1600 is out of range
+  }
+}
