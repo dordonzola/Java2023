d603e8441114203c1d711226c2b2cf9c3c6b5d22
dordonzola <dorota.jablonska@student.uj.edu.pl>
2023-11-10 20:27:41 +0100

    leapyearcalculator.java added

--- src/main/java/org/leapyear/LeapYearCalculator.java
+++ src/main/java/org/leapyear/LeapYearCalculator.java
@@ -3,7 +3,19 @@ package org.leapyear;
 public class LeapYearCalculator {
 
   public static boolean isLeapYear(int year) {
-    // Your implementation here
-    return false;
+    if (year<0){
+      return false;
+    }
+    if (year%4==0) {
+      if (year%100==0) {
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+    else {
+      return false;
+    }
   }
 }
