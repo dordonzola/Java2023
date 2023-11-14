51191eba8b94fceece1bdd268353b656b505fd98
Maciej Mozolewski <m.mozolewski@gmail.com>
2023-11-14 23:23:22 +0100

    Lab05 Generics - StudentResourceManager

---
+++ src/main/java/org/studentresource/StudentResource.java
@@ -0,0 +1,7 @@
+package org.studentresource;
+
+public interface StudentResource {
+    String getId();
+    String getName();
+    // Add other necessary methods here
+}
---
+++ src/test/java/org/studentresource/CommentableResourceTest.java
@@ -0,0 +1,24 @@
+package org.studentresource;
+
+import org.studentresource.Course;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentableResourceTest {
+    private CommentableResource commentableResource;
+
+    @BeforeEach
+    void setUp() {
+        Course course = new Course("CS101", "Introduction to Computer Science");
+        commentableResource = new CommentableResource(course);
+    }
+
+    @Test
+    void addAndGetCommentTest() {
+        String comment = "Excellent course for beginners.";
+        commentableResource.addComment(comment);
+
+        assertEquals(comment, commentableResource.getComment(), "The comment should match the added one.");
+    }
+}
